2df82bcf6333356e9b5ace24ba9e2105d399002b
diegarlin <garcialinaresdiego@gmail.com>
2023-11-16 09:41:58 +0100

    feat: restricción en el form para email

--- piezasRevive/forms.py
+++ piezasRevive/forms.py
@@ -8,6 +8,12 @@ class RegistroUsuarioForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Ya existe un usuario con este correo electrónico.')
+        return email
+    
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
