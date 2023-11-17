e1b11c7a97994fbd32824402b623c0084dd36256
diegarlin <garcialinaresdiego@gmail.com>
2023-11-17 13:39:32 +0100

    fix: Añadidas migraciones y cambiso en admin Además he cambiado la ruta de la foto del producto

--- producto/models.py
+++ producto/models.py
@@ -4,6 +4,6 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=255)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
-    imagen = models.ImageField(upload_to='productos/')
+    imagen = models.ImageField(upload_to="producto/static/producto/images")
     categoria = models.CharField(max_length=100)
     stock = models.IntegerField()
