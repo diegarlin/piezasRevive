d113b489f5c1a07505d1b684a8f7bffc7cbcb0b3
diegarlin <garcialinaresdiego@gmail.com>
2023-11-27 16:13:34 +0100

    feat: restriccion de usuarios sobre reclamacion

--- reclamaciones/urls.py
+++ reclamaciones/urls.py
@@ -7,5 +7,6 @@ app_name="reclamaciones"
 urlpatterns = [
     path('detalle_reclamacion/<int:reclamacion_id>/', views.detalle_reclamacion, name='detalle_reclamacion'),
     path("reclamaciones/<int:pedido_id>/", views.crear_reclamacion, name='crear_reclamacion'),
-
+    path('ingresar_correo/<int:pedido_id>/', views.ingresar_correo, name='ingresar_correo'),
+    path("reclamacion/<int:pedido_id>/", views.crear_reclamacion_sinloguear, name='crear_reclamacion_sinloguear'),
 ]
--- reclamaciones/views.py
+++ reclamaciones/views.py
@@ -1,38 +1,77 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Reclamacion
 from .forms import ReclamacionForm
-from pedido.models import Pedido  # Add this line to import the Pedido model
+from pedido.models import Pedido  
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.shortcuts import redirect
 
-# ... (other imports)
 
 def crear_reclamacion(request, pedido_id):
-    # Obtener el pedido asociado al ID
+    
+    pedido = get_object_or_404(Pedido, id=pedido_id)
+
+   
+    if (pedido.usuario == request.user):
+        if request.method == 'POST':
+        
+            form = ReclamacionForm(request.POST)
+            if form.is_valid():
+                usuario = pedido.usuario if pedido.usuario else User.objects.get(username='admin')
+
+                reclamacion = form.save(commit=False)
+                reclamacion.usuario = usuario
+                reclamacion.pedido = pedido
+                reclamacion.save()
+                return redirect('reclamaciones:detalle_reclamacion', reclamacion.id)
+        else:
+            
+            form = ReclamacionForm()
+    else:
+        messages.info(request, 'No tienes permisos para acceder a esta vista. Inserte el correo con el que hizo este pedido')
+        return redirect('reclamaciones:ingresar_correo', pedido_id=pedido.id)      
+
+    return render(request, 'crear_reclamacion.html', {'form': form, 'pedido': pedido})
+
+
+
+def detalle_reclamacion(request, reclamacion_id):
+    reclamacion = get_object_or_404(Reclamacion, id=reclamacion_id)
+    return render(request, 'detalle_reclamacion.html', {'reclamacion': reclamacion})
+
+def ingresar_correo(request, pedido_id):
     pedido = get_object_or_404(Pedido, id=pedido_id)
 
     if request.method == 'POST':
-        # Si se envía un formulario, procesar los datos
+        correo_ingresado = request.POST.get('correo', '')
+        if correo_ingresado == pedido.email:
+            return redirect('reclamaciones:crear_reclamacion_sinloguear', pedido_id=pedido.id)
+        else:
+            messages.error(request, 'El correo no es correcto.')
+            return redirect("/")
+    
+    
+    return render(request, 'ingresar_correo.html', {'pedido': pedido})
+
+
+def crear_reclamacion_sinloguear(request, pedido_id):
+
+    pedido = get_object_or_404(Pedido, id=pedido_id)
+
+   
+    if request.method == 'POST':
+
         form = ReclamacionForm(request.POST)
         if form.is_valid():
-            if pedido.usuario is None:
-                # Si el usuario del pedido es None, usar el usuario admin
-                usuario = User.objects.get(username='admin')
-            else:
-                # Si hay un usuario en el pedido, usar ese usuario
-                usuario = pedido.usuario
-
+            usuario = pedido.usuario if pedido.usuario else User.objects.get(username='admin')
             reclamacion = form.save(commit=False)
             reclamacion.usuario = usuario
             reclamacion.pedido = pedido
             reclamacion.save()
             return redirect('reclamaciones:detalle_reclamacion', reclamacion.id)
     else:
-        # Si es una solicitud GET, mostrar el formulario vacío
+
         form = ReclamacionForm()
+        
 
     return render(request, 'crear_reclamacion.html', {'form': form, 'pedido': pedido})
-
-
-def detalle_reclamacion(request, reclamacion_id):
-    reclamacion = get_object_or_404(Reclamacion, id=reclamacion_id)
-    return render(request, 'detalle_reclamacion.html', {'reclamacion': reclamacion})
