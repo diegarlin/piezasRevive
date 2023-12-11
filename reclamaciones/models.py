a750015bc1d3e67488a73c681dbf829175b31492
diegarlin <garcialinaresdiego@gmail.com>
2023-12-11 16:34:28 +0100

    arreglos finales

--- reclamaciones/models.py
+++ reclamaciones/models.py
@@ -6,7 +6,6 @@ from pedido.models import Pedido
 from django.contrib.auth.models import User
 
 class Reclamacion(models.Model):
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
     motivo = models.CharField(max_length=200)
     descripcion = models.TextField(default=" ")
