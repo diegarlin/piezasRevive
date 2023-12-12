90288192f6ab57c92a6eb99142450505cc75d5ad
diegarlin <garcialinaresdiego@gmail.com>
2023-12-12 13:54:31 +0100

    Cambio setting

--- proyectoPGPI/settings.py
+++ proyectoPGPI/settings.py
@@ -147,7 +147,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'piezasrevive@gmail.com'
-EMAIL_HOST_PASSWORD = 'rdwamwrwwuwbudbh'
+EMAIL_HOST_PASSWORD = 'pchh yjin fehy kcmj'
 # rdwamwrwwuwbudbh8@ outlook
 # rdwamwrwwuwbudbh gmail
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
